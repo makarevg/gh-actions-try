60dd169709f6b6dc15b767280ba7e9df26f1ab58
Evgeny Makarov <e.v.makarov@sdventures.com>
2024-08-24 12:14:40 +0200

    add postcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import autoprefixer from 'autoprefixer'
+import postcssNested from 'postcss-nested'
 
 export default defineConfig({
     plugins: [react()],
-    base: 'sign-in-example',
+    base: '/sign-in-example',
+    css: {
+        postcss:{
+            plugins: [
+                autoprefixer,
+                postcssNested,
+            ]
+        }
+    }
 })
